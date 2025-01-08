2022fa9fe38063ef0db0c03a70663e2f87a0f360
Yochem van Rosmalen <git@yochem.nl>
2025-01-08 16:33:42 +0100

    scp to server

--- utils/quant.py
+++ utils/quant.py
@@ -14,3 +14,21 @@ def total_qbits(model):
         return qbits
 
     return recursive_walk(model)
+
+def model_size(model):
+    """In bytes."""
+    def recursive_walk(module):
+        bits = []
+
+        if isinstance(module, nn.Module):
+            if hasattr(module, "qbits") and callable(getattr(module, "qbits")):
+                bits.append(module.qbits())
+            else:
+                bits.append(sum(p.numel() for p in module.parameters(recurse=False)))
+
+            for child in module.children():
+                bits.extend(recursive_walk(child))
+
+        return bits
+
+    return recursive_walk(model)
