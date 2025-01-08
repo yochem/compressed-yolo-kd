118e6972814c440401586a57ba2e7356464bcf89
Yochem van Rosmalen <git@yochem.nl>
2025-01-08 11:36:01 +0100

    Change run

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 DATA=data/bears.yaml
 EPOCHS=30
+MODELDIR=pts
 
 . ../venv/bin/activate
 
@@ -17,17 +18,18 @@ usage() {
 }
 
 [ $# -eq 0 ] && usage
+mkdir -p "$MODELDIR"
 
 case "$1" in
 	export)
 		[ $# -ne 2 ] && usage "Error: '$1' command requires a model argument."
 		model=$2
 		python export.py \
-			--data "$DATA" \
-			--weights ../../results/$model/weights/best.pt \
-			--batch-size 1 \
 			--device 0 \
 			--img-size 320 \
+			--data "$DATA" \
+			--weights "$MODELDIR/$model/weights/best.pt" \
+			--batch-size 1 \
 			--int8 \
 			--include tflite
 		;;
@@ -35,41 +37,43 @@ case "$1" in
 	kd)
 		[ $# -ne 2 ] && usage "Error: '$1' command requires a name argument."
 		python train.py \
-			--data "$DATA" \
-			--cfg Qyolov5n.yaml \
-			--weights '' \
-			--batch-size 128 \
-			--teacher_weight pts/v5m.pt \
 			--device 0 \
 			--img-size 320 \
+			--data "$DATA" \
+			--cfg data/Qyolov5n.yaml \
+			--weights '' \
+			--batch-size 128 \
+			--teacher_weight "$MODELDIR/v5m.pt" \
 			--epochs "$EPOCHS" \
 			--exist-ok \
 			--name "$2"
+		cp -f "runs/train/$2/weights/best.pt" "$MODELDIR/$2.pt"
 		;;
 
 	single)
 		[ $# -lt 2 ] && usage "Error: '$1' command requires a name argument."
 		[ $# -lt 3 ] && usage "Error: '$1' command requires a model argument."
 		python train.py \
+			--device 0 \
+			--img-size 320 \
 			--data "$DATA" \
 			--cfg $3 \
 			--weights '' \
 			--batch-size 128 \
-			--device 1 \
-			--img-size 320 \
 			--epochs "$EPOCHS" \
 			--exist-ok \
 			--name "$2"
+		cp -f "runs/train/$2/weights/best.pt" "$MODELDIR/$2.pt"
 		;;
 
 	val)
 		[ $# -ne 2 ] && usage "Error: '$1' command requires a model argument."
 		model=$2
 		python val.py \
-			--data "$DATA" \
-			--weights ../../results/$model/weights/best.pt \
 			--device 0 \
 			--img-size 320 \
+			--data "$DATA" \
+			--weights "$MODELDIR/$model.pt" \
 			--task test \
 			--name "$model" \
 			--batch-size 128
--- scp.sh
+++ scp.sh
@@ -5,7 +5,6 @@ changed=$(git status --porcelain | cut -c 4-)
 scp_path="Yochem@cpu8gpu2:~/yolokd/compressed-yolo-kd"
 
 for file in $changed; do
-	echo "Copying $file"
 	gcloud compute scp "$file" "$scp_path/$file" --recurse --zone "europe-west4-c"
 	git add "$file"
 done
