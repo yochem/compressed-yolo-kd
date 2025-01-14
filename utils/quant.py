c11c76d12fa5325b9396f51f1068e68563613bed
Yochem van Rosmalen <git@yochem.nl>
2025-01-14 10:44:30 +0100

    scp to server

--- utils/quant.py
+++ utils/quant.py
@@ -18,13 +18,14 @@ def total_qbits(model):
 
 
 def size_per_layer(model):
-    return [sum(model_size(layer)) for layer in model.children()]
+    # return [sum(model_size(layer)) for layer in model.children()]
     s = 0
     for l in model.children():
         try:
             s += sum(model_size(l))
         except TypeError:
             print(l)
+    return s
 
 
 def model_size(model):
