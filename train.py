b89ca41fbd8a2d5c683e3ab8f6205252bf81d3eb
Yochem van Rosmalen <git@yochem.nl>
2025-01-23 15:25:36 +0100

    scp to server

--- train.py
+++ train.py
@@ -579,7 +579,6 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 T = 4.0
                 alpha = 0.5
                 for model_idx, m in enumerate(models):
-                    print(m)
                     ce_loss, items = compute_loss(preds[model_idx], targets)
                     if loss_items is None:
                         loss_items = items
